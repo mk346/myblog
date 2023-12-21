aaa32eac04d46cd3343366c346a1d1ac243bc5d8
mk346 <calebkinyuamunene@gmail.com>
2023-12-21 18:42:25 +0300

    update-1

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+// import React from 'react'
+import Feed from '@components/Feed'
 
 const Home = () => {
   return (
-    <div>page</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share
+        <br className='max-md:hidden'/>
+        <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+        </h1>
+        <p className='desc text-center'>This is a blog about Popular AI prompting tool for modern world to discover, create adn share creative prompts</p>
+
+        {/* feed component */}
+        <Feed/>
+    </section>
   )
 }
 
