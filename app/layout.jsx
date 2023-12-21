c0beed52cff1774f36a64c30f571c6db4458f302
mk346 <calebkinyuamunene@gmail.com>
2023-12-21 09:28:49 +0300

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/global.css';
+
+
+export const metadata = {
+    title: "My Blog",
+    descripton: 'Discover $ Share AI Prompts',
+}
+
+const RootLayout = ({ children }) => {
+  return (
+      <html lang='en'>
+          <body>
+              <div className='main'>
+                  <div className='gradient'/>
+              </div>
+              <main className='app'>
+                  { children }
+              </main>
+          </body>
+          
+    </html>
+  )
+}
+
+export default RootLayout
